bca0ed34fc41dc1c7b312ce70aaccccb2e648ecd
Logan Sauer <ldsauer2@gmail.com>
2025-01-07 11:34:41 -0700

    comitting completed iteration 1-3 and part of 4

--- lib/election.rb
+++ lib/election.rb
@@ -1,2 +1,24 @@
 class Election
+    attr_reader :year, :races
+
+    def initialize(year)
+        @year = year
+        @races = []
+    end
+
+    def add_race(race)
+        @races << race
+    end
+
+    def candidates
+        @races.map { |race| race.candidates }.flatten
+    end
+
+    def vote_counts
+        counts = {}
+        candidates.each do |candidate|
+            counts[candidate.name] = candidate.votes
+        end
+        counts
+    end
 end
