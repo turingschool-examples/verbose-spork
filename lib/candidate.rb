861ac647eca6f9da2d2902d72d6856175f54244e
Sebastian McKee <sebmcke@gmail.com>
2025-01-07 10:10:33 -0600

    Add initialize function to democrat

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,8 @@
 class Candidate
+    attr_reader :name, :party
+
+    def initialize(info_hash)
+        @name = info_hash[:name]
+        @party = info_hash[:party]
+    end
 end
---
+++ spec/candidate_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require './lib/candidate'
+
+describe Candidate do
+    before do
+        @diana = Candidate.new({name: "Diana D", party: :democrat})
+    end
+
+    describe '#initialize' do
+        it 'exists' do
+            expect(@diana).to be_a(Candidate)
+        end
+
+        it 'has a name' do
+            expect(@diana.name).to eq("Diana D")
+        end
+
+        it 'has a party' do
+            expect(@diana.party).to eq(:democrat)
+        end
+    end
+end
