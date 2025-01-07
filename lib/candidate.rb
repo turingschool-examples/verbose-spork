bc0b0675f2e39207dce1d03da0bf7ca1a52fae64
Mark Kendall <mkendall.tutor@gmail.com>
2025-01-07 10:24:49 -0600

    Add Candidate class and associated tests

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,15 @@
 class Candidate
+  attr_reader :name, :party, :votes
+
+  def initialize(input_hash)
+    @name = input_hash[:name]
+    @party = input_hash[:party]
+
+    @votes = 0
+  end
+
+  def vote_for
+    @votes += 1
+  end
+
 end
