6ce4c4be3acf4fa30bb89fc7e8e03df3aec261dc
Jaren Garman <jarengarman@protonmail.com>
2025-01-07 11:08:17 -0500

    test: Add tests for Candidate initialize method

--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -1,3 +1,23 @@
 # frozen_string_literal: true
 
 require_relative 'spec_helper'
+
+RSpec.describe Candidate do
+  subject(:diana) { described_class.new({ name: 'Diana D', party: :democrat }) }
+
+  describe '#initialize' do
+    it { is_expected.to be_instance_of described_class }
+
+    it 'has a name' do
+      expect(diana.name).to eq('Diana D')
+    end
+
+    it 'has a party' do
+      expect(diana.party).to eq(:democrat)
+    end
+
+    it 'has zero votes' do
+      expect(diana.votes).to eq(0)
+    end
+  end
+end
