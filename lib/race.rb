fd4f034c430f48522779ce144751fd6aa24708f8
Donte Handy <donte.handy@gmail.com>
2025-01-07 12:21:27 -0400

    Implement Race class with attributes and candidate registration functionality; add RSpec tests

--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,16 @@
+require './lib/candidate'
+
 class Race
+  attr_reader :office, :candidates
+
+  def initialize(office)
+    @office = office
+    @candidates = []
+  end
+
+  def register_candidate(details) #accepts a hash of details from candidate class
+    candidate = Candidate.new(details)
+    @candidates << candidate
+    candidate
+  end
 end
---
+++ spec/race_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require './lib/race'
+
+RSpec.describe Race do
+  it 'exists and has attributes' do
+    race = Race.new("Texas Governor")
+
+    expect(race).to be_a(Race)
+    expect(race.office).to eq("Texas Governor")
+    expect(race.candidates).to eq([])
+  end
+
+  it 'can register candidates' do
+    race = Race.new("Texas Governor")
+
+    candidate1 = race.register_candidate({name: "Diana D", party: :democrat}) #name and party are keys in the details hash votes default to 0
+    expect(candidate1).to be_a(Candidate)
+    expect(candidate1.name).to eq("Diana D")
+    expect(candidate1.party).to eq(:democrat)
+
+    candidate2 = race.register_candidate({name: "Roberto R", party: :republican})
+    expect(candidate2).to be_a(Candidate)
+    expect(candidate2.name).to eq("Roberto R")
+    expect(candidate2.party).to eq(:republican)
+
+    expect(race.candidates).to eq([candidate1, candidate2])
+  end
+end
