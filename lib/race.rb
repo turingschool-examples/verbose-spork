1890419e609690d0b24e8acea3809e19b6eb7906
Jaren Garman <jarengarman@protonmail.com>
2025-01-07 11:24:00 -0500

    feat: Add register_candidate method

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'candidate'
+
 # Create a race for the candidates to run in
 class Race
   attr_reader :office, :candidates
@@ -7,5 +9,9 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+  end
+
+  def register_candidate(candidate_details)
+    (@candidates << Candidate.new(candidate_details)).last
   end
 end
