0baa692da533413d357deeab976aab08ce9ef260
Jacob Inman <jrinman6295@gmail.com>
2025-01-07 08:18:42 -0800

    candidate class initialize written and passed

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,3 +1,11 @@
 class Candidate
+    attr_reader :name, :party, :votes
+
+    def initialize(candidate_details)
+        @name = candidate_details[:name]
+        @party = candidate_details[:party]
+        @votes = 0
+    end
+
 
 end
