bac268820a11a1c59cd7d83b1cbaccc44e5c3602
Jaren Garman <jarengarman@protonmail.com>
2025-01-07 11:46:06 -0500

    test: Add tests for candidates method

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -15,17 +15,36 @@ RSpec.describe Election do
     it 'has no races' do
       expect(election.races).to eq([])
     end
+
+    it 'has no candidates' do
+      expect(election.candidates).to eq([])
+    end
   end
 
   describe '#add_race' do
-    let(:first_race) { instance_double(Race, office: 'Texas Governor') }
-    let(:second_race) { instance_double(Race, office: 'Texas Senator') }
+    let(:first_race) { instance_double(Race) }
+    let(:second_race) { instance_double(Race) }
 
     it 'can add races' do
       election.add_race first_race
       election.add_race second_race
 
       expect(election.races).to eq([first_race, second_race])
+    end
+  end
+
+  describe '#candidates' do
+    let(:first_race) { instance_double(Race) }
+    let(:second_race) { instance_double(Race) }
+    let(:first_candidate) { instance_double(Candidate) }
+    let(:second_candidate) { instance_double(Candidate) }
+    let(:third_candidate) { instance_double(Candidate) }
+
+    allow(first_race).to receive(:candidates).and_return([first_candidate, second_candidate])
+    allow(second_race).to receive(:candidates).and_return([third_candidate])
+
+    it 'can get all candidates' do
+      expect(election.candidates).to eq([first_candidate, second_candidate, third_candidate])
     end
   end
 end
