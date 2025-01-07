f656088ebd8701faab8ad74581439d7618fbc850
Jacob Inman <jrinman6295@gmail.com>
2025-01-07 09:09:24 -0800

    election initialize complete

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -6,10 +6,17 @@ require './lib/election'
 
 RSpec.describe Election do
     before(:each) do
-
+        @election = Election.new(2000)
     end
 
     describe 'initialize' do
-        expect(@)
+        it 'exists' do
+            expect(@election).to be_an_instance_of(Election)
+        end
+
+        it 'can tell year and races within year' do
+            expect(@election.year).to eq(2000)
+            expect(@election.races).to eq([])
+        end
     end
 end
