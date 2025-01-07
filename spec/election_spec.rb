7bb8f6b6af7ed2df88d9553be83b62b1b826fef7
Jaren Garman <jarengarman@protonmail.com>
2025-01-07 11:32:37 -0500

    test: Add other initialize tests for Election

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -3,9 +3,17 @@
 require_relative 'spec_helper'
 
 RSpec.describe Election do
-  subject(:election) { described_class.new(2024) }
+  subject(:election) { described_class.new('2024') }
 
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
+
+    it 'has a year' do
+      expect(election.year).to eq('2024')
+    end
+
+    it 'has no races' do
+      expect(election.races).to eq([])
+    end
   end
 end
