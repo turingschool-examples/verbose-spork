5d92aaa611f73834d08850167d42da85aaabafd6
ontruster74 <ontruster74@gmail.com>
2025-01-07 09:31:27 -0700

    Initial Commit

--- lib/election.rb
+++ lib/election.rb
@@ -1,2 +1,30 @@
 class Election
+  attr_reader :year, :races
+
+  def initialize(year)
+    @year = year
+    @races = []
+  end
+
+  def add_race(race)
+    @races << race
+  end
+
+  def candidates
+    candidates = []
+    @races.each do |race|
+      @race.candidates.each do |candidate|
+        candidates << candidate
+      end
+    end 
+    return candidates
+  end
+
+  def vote_counts
+    vote_counts = {}
+    self.candidates.each do |candidate|
+      vote_counts[candidate.name] = candidate.votes
+    end
+    return vote_counts
+  end
 end
--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,14 @@
 class Race
+  attr_reader :office, :candidates
+
+  def initialize(office)
+    @office = office
+    @candidates = []
+  end
+
+  def register_candidate(candidate_data)
+    @candidates << candidate_data
+    return candidate_data
+  end
+
 end
