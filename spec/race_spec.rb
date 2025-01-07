ca789b4078ab9da9855ae499aaa812405c9f88fc
Chase Phillips <chase.thomas.phillips@gmail.com>
2025-01-07 12:00:04 -0500

    created register candidate method and tests as listed in the interaction pattern

---
+++ spec/race_spec.rb
@@ -0,0 +1,31 @@
+require './lib/candidate'
+require './lib/race'
+
+RSpec.describe Race do
+  before(:each) do
+    @race = Race.new("Texas Governor")
+  end
+
+  it "exists" do
+    expect(@race).to be_an_instance_of(Race)
+
+  end
+
+  it "has office and candidate attributes" do
+    expect(@race.office).to eq("Texas Governor")
+    expect(@race.candidates).to eq([])
+  end
+
+  it "can register a candidate and returns instance of Candidate" do
+    candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
+    expect(candidate1.class).to eq(Candidate)  
+    expect(candidate1.name).to eq("Diana D")
+    expect(candidate1.party).to eq(:democrat)
+
+    candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
+    expect(candidate2.class).to eq(Candidate)
+    expect(candidate2.name).to eq("Roberto R")
+    expect(candidate2.party).to eq(:republican)
+  end
+
+end
