614b1649b6be6fb8faa6f65f4977a0ecae243879
Jaren Garman <jarengarman@protonmail.com>
2025-01-07 11:10:00 -0500

    feat: Add initialize method for Candidate

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# Create a candidate to run in the election
 class Candidate
+  attr_reader :name, :party, :votes
+
+  def initialize(candidate_details)
+    @name = candidate_details[:name]
+    @party = candidate_details[:party]
+    @votes = 0
+  end
 end
