52e5d2d3ee3f2f846412af25fdf010e549cd5302
Jacob Inman <jrinman6295@gmail.com>
2025-01-07 09:06:25 -0800

    election class outlined

--- lib/election.rb
+++ lib/election.rb
@@ -1,2 +1,20 @@
 class Election
+    attr_reader :year, :races
+
+    def initialize(year)
+        @year = year
+        @races = []
+    end
+
+    # def add_race
+    #     <<
+    # end
+
+    # def candidates
+    #     []
+    # end
+
+    # def vote_counts
+    #     {}
+    # end
 end
---
+++ spec/election_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'pry'
+require './lib/candidate'
+require './lib/race'
+require './lib/election'
+
+RSpec.describe Election do
+    before(:each) do
+
+    end
+
+    describe 'initialize' do
+        expect(@)
+    end
+end
